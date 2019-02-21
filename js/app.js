9bbe547320edb3316083c68f7f3b54d62650d422
simonpod <s.podboraczynski@gmail.com>
2019-02-21 02:18:39 +0000

    more comments to create game logic

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // enemy positin and sprite image
     this.x = 0;
     this.y = 0;
     this.sprite = 'images/enemy-bug.png';
@@ -16,6 +12,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // change x  * speed *dt until touched border
+    //if touched border move to initial x
 };
 
 // Draw the enemy on the screen, required method for game
@@ -23,17 +21,24 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// My player class
 var Player = function () {
     this.x = 0;
     this.y = 0;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// This class requires an update(), render() and
-// a handleInput() method.
+//  update() method to update playes position on boad
+   // check player position against enemies
+   // check if player reached water (win)
+      // if any above true move player to beginning - start method
+
+
+//this methd renders player positin on the board. It is the same as the one povided for Enemy object
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+//   handleInput() method to handle keyboad input
 
 
 // Now instantiate your objects.
