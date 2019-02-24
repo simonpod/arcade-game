5b703bee0fa22045df9d8404da02017e1031959e
simonpod <s.podboraczynski@gmail.com>
2019-02-24 22:02:26 +0000

    made enemy move

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(speed) {
     // enemy positin and sprite image
     this.x = 0;
     this.y = 0;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
-
+    this.move = 101;
 };
 
 
@@ -15,6 +16,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x< 4 *this.move) {
+     this.x += 30 * dt;
+    }
     // change x  * speed *dt until touched border
     //if touched border move to initial x
 };
--- js/engine.js
+++ js/engine.js
@@ -90,10 +90,10 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-       /*allEnemies.forEach(function(enemy) {
+       allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        player.update();*/
+       /* player.update();*/
     }
 
     /* This function initially draws the "game level", it will then call
